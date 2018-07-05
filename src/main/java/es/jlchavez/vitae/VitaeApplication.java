a16649a800e26723ac165644cc2ed57de1e743bb
Juan Luis Chávez Lobato <juanluis.chavez78@gmail.com>
2018-07-05 11:52:54 +0200

    Comment test

--- src/main/java/es/jlchavez/vitae/VitaeApplication.java
+++ src/main/java/es/jlchavez/vitae/VitaeApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*
+Comment test
+*/
 public class VitaeApplication {
 
 	public static void main(String[] args) {
